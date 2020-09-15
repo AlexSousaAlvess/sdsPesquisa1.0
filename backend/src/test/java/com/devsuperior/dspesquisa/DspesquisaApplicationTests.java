6eb6df37b1cf55417ad9ef9b24469098246ffa95
AlexSousaAlvess <alexsousaalves317@gmail.com>
2020-09-15 17:43:45 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/dspesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
